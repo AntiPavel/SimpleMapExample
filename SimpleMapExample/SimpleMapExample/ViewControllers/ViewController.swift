8454335f77a3ff8d2e800e22ab9cf7f32d2588c3
pavel <pavel.antonov1980@icloud.com>
2019-08-12 13:19:17 +0200

    base project setup

---
+++ SimpleMapExample/SimpleMapExample/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SimpleMapExample
+//
+//  Created by paul on 12/08/2019.
+//  Copyright © 2019 pavel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
